663f478c8a20716c3f8fc8eb7c8ee719caf67260
angelDLD <danielleyvareyez@gmail.com>
2025-12-18 18:53:39 -0600

    V1.0 setup Manager

---
+++ src/main/java/mx/angelDL_/gradleCore/GradleCore.java
@@ -0,0 +1,17 @@
+package mx.angelDL_.gradleCore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GradleCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
