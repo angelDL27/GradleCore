4645343271098f3be7860f4a1d2f5a309a0f301b
angelDLD <danielleyvareyez@gmail.com>
2025-12-18 20:00:49 -0600

    new V1.0 setup Multiversion MC 1.8 - 1.21

---
+++ public-api/src/main/java/mx/angelDL_/publicApi/PublicApi.java
@@ -0,0 +1,17 @@
+package mx.angelDL_.publicApi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PublicApi extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
